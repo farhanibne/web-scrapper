70427403f0f9ffc8b21d2dcda35d7a9bcdf8a31e
farhan ibne saif <farhanibne860@gmail.com>
2022-10-22 13:56:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express');
 
 const app = express();
 
-const url = 'https://www.munnasacademy.com/index.php'
+const url = '##WEBSITE_NAME##
 axios(url).then(response => {
     const html = response.data;
     const $ = cheerio.load(html);
